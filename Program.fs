105beb2db062e4072d40e06a480fb59b0641e059
Your Name <you@example.com>
2025-10-02 12:57:13 +0200

    My first Fs code

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿open System    
+[<EntryPoint>]
+let main argv =
+    printfn "Hello world F#\n"
+    
+    
+    printf "Enter your name: \n"
+    let name:string = Console.ReadLine()
+    printfn "Hello, %s!" name
+        
+          
+    0 // return an integer exit code
